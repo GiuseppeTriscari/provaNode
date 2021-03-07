389042d95a9c42a7d909ac8f1a5f5add6a796b40
GiuseppeTriscari <giuseppe.triscari97@gmail.com>
2021-03-07 17:12:59 +0100

    ok

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*
 var http = require("http");
 
 //express
@@ -39,3 +40,25 @@ apiserver.get("/mionome", function(req, res){
 
 //get SOMMA
 apiserver.get("/somma")
+
+*/
+
+var http = require("http");
+
+var express = require("express");
+var apiServer = express();
+
+var port = 3000;
+
+apiServer.listen(port, () => {
+    console.log("server running on port ", port);
+});
+
+apiServer.get("/nome", (req, res) =>{
+    console.log("ho ricevuto una get su nome");
+    res.send("ciao, il nome del server è: NODEPOGLIANI");
+});
+
+apiServer.get("/", function(req, res){
+    res.send("sei in home");
+})
