6d5a77598ece0a1c5e34d8ce9c658541f4b17f40
GiuseppeTriscari <giuseppe.triscari97@gmail.com>
2021-03-07 17:58:54 +0100

    sa

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var http = require("http");
 
 var express = require("express");
+const { title } = require("process");
 var apiServer = express();
 
 
 var port = 3000;
 
+title='NodeTriscari';
 apiServer.listen(port, () => {
     console.log("server running on port ", port);
 });
@@ -21,4 +23,4 @@ apiServer.get("/", function(req, res){
     res.send('<h2><label style="background-color:lightgreen;">Sei in home!</label></h2>');
 });
 
-document.title='NodeTriscari';
+
