3f8435607bbbc25a907c1847b96b5e7aa50e4e4e
GiuseppeTriscari <giuseppe.triscari97@gmail.com>
2021-03-07 17:52:21 +0100

    asd

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ apiServer.listen(port, () => {
 });
 
 apiServer.get("/nome", (req, res) =>{
-    document.title="NodeTriscari";
+    document.title='NodeTriscari';
     console.log("ho ricevuto una get su nome");
     res.send('<h2><label style="background-color:lightblue;">Ciao, il nome del server è: NODETRISCARI</label></h2>');
 });
 
 apiServer.get("/", function(req, res){
-    document.title="NodeTriscari";
+    document.title='NodeTriscari';
     res.send('<h2><label style="background-color:lightgreen;">Sei in home!</label></h2>');
 })
