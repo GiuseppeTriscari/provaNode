2f2c8b42935549f6ee01e6e67b4cbb424b1c8967
GiuseppeTriscari <giuseppe.triscari97@gmail.com>
2021-03-07 18:05:38 +0100

    asd

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+var operations = function() {
+
+    document.title='NodeTriscari';
+
+}
+
 var http = require("http");
 
 var express = require("express");
@@ -9,19 +15,19 @@ var port = 3000;
 
 
 apiServer.listen(port, () => {
-    document.title='NODETRISCARI';
+    
     console.log("server running on port ", port);
 });
 
 apiServer.get("/nome", (req, res) =>{
-    //document.title='NodeTriscari';
     console.log("ho ricevuto una get su nome");
     res.send('<h2><label style="background-color:lightblue;">Ciao, il nome del server è: NODETRISCARI</label></h2>');
 });
 
 apiServer.get("/", function(req, res){
-    //document.title='NodeTriscari';
     res.send('<h2><label style="background-color:lightgreen;">Sei in home!</label></h2>');
 });
 
 
+
+document.addEventListener("DOMContentLoaded", operations);
