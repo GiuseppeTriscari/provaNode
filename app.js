d76e36a5f799a6d0494f345cf6de22568b12a15b
Triscari Giuseppe <triscari.giuseppe@osdbsesto>
2021-04-13 13:20:34 +0200

    ok

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ apiServer.listen(port, () => {
     console.log("server running on port ", port);
 });
 
+apiServer.get("/", function(req, res){
+    res.send('<h2><label style="background-color:lightgreen;">Sei in home!</label></h2>');
+});
+
 apiServer.get("/nome", (req, res) =>{
     console.log("ho ricevuto una get su nome");
     res.send('<h2><label style="background-color:lightblue;">Ciao, il nome del server è: Giuseppe</label></h2>');
@@ -38,6 +42,10 @@ apiServer.get("/login" , (req, res) => {});
 apiServer.post("/login" , (req, res) => {}); 
 
 
-apiServer.get("/", function(req, res){
-    res.send('<h2><label style="background-color:lightgreen;">Sei in home!</label></h2>');
+
+apiServer.get("/voti" , (req, res) => {
+    var voti = [{"data" : "15/11/2020", "voti" : "6", "descrizione" : "matematica"}];
+
+    res.send('<embed src="voti.html">')
 });
+
